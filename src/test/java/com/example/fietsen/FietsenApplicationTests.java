3bc2db8a7f0506f812c494e29a8d4d39e737badf
Lightflamingo <Lightflamingowilreborn@gmail.com>
2022-08-23 12:44:23 +0200

    Initial commit

---
+++ src/test/java/com/example/fietsen/FietsenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fietsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FietsenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
